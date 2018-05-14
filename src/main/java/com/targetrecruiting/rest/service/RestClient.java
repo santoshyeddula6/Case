265a6c2155865d6a1144aaa79dd952c5b3504ea2
w02029 <rajasantoshreddy.yeddula@delta.com>
2018-05-14 14:58:45 -0400

    changes to restclient with using resttemplate

--- src/main/java/com/targetrecruiting/rest/service/RestClient.java
+++ src/main/java/com/targetrecruiting/rest/service/RestClient.java
@@ -3,35 +3,37 @@ package com.targetrecruiting.rest.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import com.targetrecruiting.rest.model.Product;
+import com.targetrecruiting.rest.model.ProductData;
 
 @Service
 public class RestClient {
-	
+	private static final Logger log = LogManager.getLogger(RestClient.class);
 	private final RestTemplate restTemplate;
-    
-	
+
 	public RestClient(RestTemplateBuilder builder) {
 		this.restTemplate = builder.build();
 	}
-	
-	public Product getProductName(String  byId) {
-		String url = "http://redsky.target.com/v2/pdp/tcin/{id}";
-		System.out.println("In ProductName");
-		System.out.println(this.restTemplate.toString());
-		Map<String,String> uriParams = new HashMap<String,String>();
+
+	public ProductData getProductName(String url, String byId) {
+		log.debug("getProductName : Received productId- "+byId);
+		//String url = "https://redsky.target.com/v2/pdp/tcin/{id}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics,deep_red_labels";
+		Map<String, String> uriParams = new HashMap<String, String>();
 		uriParams.put("id", byId);
-		
-		UriComponentsBuilder uri = UriComponentsBuilder.fromUriString(url).queryParam("excludes","taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics,deep_red_labels"); 
-				//"taxonomy","price","promotion","bulk_ship","rating_and_reviews","rating_and_review_statistics","question_answer_statistics");
-		System.out.println(uri.buildAndExpand(uriParams).toUriString());
-		Product dt = this.restTemplate.getForObject(uri.buildAndExpand(uriParams).toUriString(),Product.class);
-	    return dt;
+		ResponseEntity<ProductData> response = this.restTemplate.exchange(url, HttpMethod.GET, null, ProductData.class,
+				uriParams);
+		log.debug("getProductName : status code "+response.getStatusCode());
+		ProductData dt = response.getBody();
+		log.debug("getProductName : Product data from response - "+dt.toString());
+		return dt;
 	}
 
 }
