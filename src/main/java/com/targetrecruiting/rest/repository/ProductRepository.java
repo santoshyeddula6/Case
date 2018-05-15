e893fd854b3d9b15ca8369523934ea4b89f306a5
santoshyeddula6 <santosh.yeddula6@gmail.com>
2018-05-15 01:55:23 -0400

    Added few methods

--- src/main/java/com/targetrecruiting/rest/repository/ProductRepository.java
+++ src/main/java/com/targetrecruiting/rest/repository/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.targetrecruiting.rest.repository;
 
-import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -9,6 +8,9 @@ import com.targetrecruiting.rest.model.ProductPrice;
 
 @Repository
 public interface ProductRepository extends MongoRepository<ProductPrice,String> {
-
-	public Optional<ProductPrice> findById(String id); 
+	
+	public boolean existsByProductId(String id);
+	
+	public ProductPrice findByProductId(String id);
+	
 }
